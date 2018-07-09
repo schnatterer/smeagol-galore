e7c855b585e86332ab1141622dcb2122b77f3ce3
schnatterer <johannes@schnatterer.info>
2018-07-09 22:22:38 +0200

    Test: Tries to poll SCMM, waiting to become ready.
    Does not work, because /scm gives positive response before /scm/api returns non-401 response...

--- test.sh
+++ test.sh
@@ -3,20 +3,37 @@
 set -o errexit -o nounset -o pipefail
 
 main() {
+
+    waitForScm
+
     validateScmRestApiUsesCas
 
     validateSmeagolUsesCas
 }
 
+waitForScm() {
+
+    # This takes 200s and more because of the installed plugins on first start
+    for i in {1..30} # Don't wait forever
+    do
+        ret=$(curl -sS --insecure --connect-timeout 1 https://localhost:8443/scm 2>&1 || true)
+        echo "Waiting for SCMM returned: ${ret}"
+        if [ -z "${ret}" ]; then
+            echo "SCMM became ready"
+            break
+        fi
+        sleep 10
+
+        if [ "$i" = "30" ]; then 
+            echo "Cancel waiting for SCMM"
+            return 1
+         fi
+    done
+}
 
 validateScmRestApiUsesCas() {
 
-    queryUsers="curl -su admin:admin --retry 3 --retry-delay 0 --insecure https://localhost:8443/scm/api/rest/users.json"
-    # For some reasons this works only on the 3rd try when using SCMM's Rest API without accessing the UI. So do a little "warmup"
-    ignore=$(eval ${queryUsers})
-    ignore=$(eval ${queryUsers})
-    # Do the actual check
-    result=$(eval ${queryUsers})
+    result=$(curl -su admin:admin --retry 3 --retry-delay 0 --insecure https://localhost:8443/scm/api/rest/users.json 2>&1)
     echo "Query Users = ${result}"
     # Validate our admin user is returned
     echo ${result} | grep scm@adm.in

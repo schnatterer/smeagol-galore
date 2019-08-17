57bb1d517928fa41b278e79108287cf1d58ed6fc
Johannes Schnatterer <johannes@schnatterer.info>
2019-08-17 15:32:44 +0200

    Adapts SCM init scripts to v2

--- scm/opt/scm-server/init.script.d/020-configuration.groovy
+++ scm/opt/scm-server/init.script.d/020-configuration.groovy
@@ -1,34 +1,34 @@
-import sonia.scm.config.ScmConfiguration
-import sonia.scm.util.ScmConfigurationUtil
-import groovy.json.JsonSlurper
-
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import sonia.scm.config.ScmConfiguration
+import sonia.scm.group.Group
+import sonia.scm.group.GroupManager
+import sonia.scm.security.PermissionAssigner
+import sonia.scm.security.PermissionDescriptor
+import sonia.scm.util.ScmConfigurationUtil
 
 def logger = LoggerFactory.getLogger("sonia.scm.configuration.groovy")
 
-def config = injector.getInstance(ScmConfiguration.class)
-def env = System.getenv()
-
-// set admin group
-if (config.getAdminGroups() == null) {
-    config.setAdminGroups(new HashSet())
-} else {
-    config.getAdminGroups().clear()
-}
-
-String adminGroup = env['ADMIN_GROUP']
-if (adminGroup != null && adminGroup.length() > 0) {
-    logger.info("Adding admin group ${adminGroup}")
-    config.getAdminGroups().add(adminGroup)
-} else {
-    logger.warn("admin Group emtpy. None set.")
-}
-
+def config = injector.getInstance(ScmConfiguration.class);
+config.setNamespaceStrategy("CustomNamespaceStrategy");
 // set base url
 String fqdn = env['FQDN']
 logger.info("Setting FQDN ${fqdn}")
 config.setBaseUrl("https://${fqdn}/scm")
-
 // store configuration
-ScmConfigurationUtil.getInstance().store(config)
+ScmConfigurationUtil.getInstance().store(config);
+
+// set admin group
+String adminGroup = env['ADMIN_GROUP'];
+GroupManager groupManager = injector.getInstance(GroupManager.class);
+
+Group group = groupManager.get(adminGroup);
+if (group == null) {
+    group = new Group("cas", adminGroup);
+    group.setExternal(true);
+
+    group = groupManager.create(group);
+}
+
+PermissionAssigner permissionAssigner = injector.getInstance(PermissionAssigner.class);
+PermissionDescriptor descriptor = new PermissionDescriptor("*");
+permissionAssigner.setPermissionsForGroup(adminGroup, Collections.singleton(descriptor));
--- scm/opt/scm-server/init.script.d/060-cas-configuration.groovy
+++ scm/opt/scm-server/init.script.d/060-cas-configuration.groovy
@@ -1,23 +1,13 @@
-import groovy.json.JsonSlurper
-
-def env = System.getenv()
 
 try {
-    def cas = injector.getInstance(Class.forName("de.triology.scm.plugins.cas.CasAuthenticationHandler"))
-        def config = cas.getConfig()
+    def cas = injector.getInstance( Class.forName("com.cloudogu.scm.cas.CasContext", true, Thread.currentThread().getContextClassLoader()) );
+    def config = cas.get();
 
-        String fqdn = env['FQDN']
-        config.setCasServerUrl("https://${fqdn}/cas")
+    String fqdn = env['FQDN']
+    config.setCasUrl("https://${fqdn}/cas");
+    config.setEnabled(true);
 
-        config.setCasAttrUsername("username")
-        config.setCasAttrDisplayName("displayName")
-        config.setCasAttrMail("mail")
-        config.setCasAttrGroup("groups")
-
-        config.setTolerance("5000")
-        config.setEnabled(true)
-
-    cas.storeConfig(config)
-} catch( Exception e ) {
+    cas.set(config);
+} catch (ClassNotFoundException ex) {
     println "cas plugin seems not to be installed"
 }

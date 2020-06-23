815a4353deb89bf065f922ff41cb8029835e3591
Johannes Schnatterer <johannes@schnatterer.info>
2020-06-23 23:02:17 +0200

    First version with built-in letsencrypt support

--- tomcat/src/main/java/info/schnatterer/tomcat/Main.java
+++ tomcat/src/main/java/info/schnatterer/tomcat/Main.java
@@ -8,14 +8,21 @@ import java.io.File;
 
 public class Main {
 
-    public static final String CERT_FOLDER = "/config/certs";
-    public static final String PK = CERT_FOLDER +"/pk.pem";
-    public static final String CRT = CERT_FOLDER + "/crt.pem";
-    public static final String CA = CERT_FOLDER + "/ca.crt.pem";
+    public static final String DOMAIN = System.getenv("DOMAIN");
+    public static final String CERT_FOLDER = System.getenv("CERT_DIR");
+    public static final String PK = CERT_FOLDER + '/' + DOMAIN + "/privkey.pem";
+    public static final String CRT = CERT_FOLDER + '/' + DOMAIN + "/cert.pem";
+    public static final String CA = CERT_FOLDER + '/' + DOMAIN + "/fullchain.pem";
+
+    public static final String DEFAULT_HTTP_PORT = "8080";
+    public static final String DEFAULT_HTTPS_PORT = "8443";
+
 
     public static void main(String[] args) throws Exception {
 
         Tomcat tomcat = new Tomcat();
+        String httpPort = System.getProperty("http.port", DEFAULT_HTTP_PORT);
+        String httpsPort = System.getProperty("https.port", DEFAULT_HTTPS_PORT);
         String catalinaHome = "/tomcat";
         tomcat.setBaseDir(new File(catalinaHome).getAbsolutePath());
 
@@ -27,9 +34,9 @@ public class Main {
         tomcat.addWebapp("/smeagol", new File(catalinaHome + "/webapps/smeagol").getAbsolutePath());
         tomcat.addWebapp("/scm", new File(catalinaHome + "/webapps/scm").getAbsolutePath());
 
-        ReloadingTomcatConnectorFactory.addHttpsConnector(tomcat, Integer.parseInt(System.getProperty("https.port")), PK, CRT, CA);
-        
-        addHttpConnector(tomcat, Integer.parseInt(System.getProperty("http.port")));
+        ReloadingTomcatConnectorFactory.addHttpsConnector(tomcat, Integer.parseInt(httpsPort), PK, CRT, CA);
+
+        addHttpConnector(tomcat, Integer.parseInt(httpPort));
 
         tomcat.start();
         tomcat.getServer().await();
